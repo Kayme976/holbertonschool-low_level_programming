ff26232b2d9484ddb9cf6c6167218d78b1d1f73a
Kayme976 <karim.soumbou@gmail.com>
2023-10-16 05:47:23 -0700

    enemies

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -12,15 +12,15 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
 	{
-		printf("%d is positive \n",n );
+		printf("%d is positive\n",);
 	}
         if (n < 0)
 	{
-       		printf("%d is negative \n",n );
+       		printf("%d is negative\n", n);
 	}
 	if (n == 0)
 	{
-		printf("%d is zero \n",n );
+		printf("%d is zero\n", n);
 	}
 	return (0);
 }
