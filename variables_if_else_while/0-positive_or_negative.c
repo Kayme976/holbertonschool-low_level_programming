a768beed8742bee2072a095657f9164556cab5f6
Kayme976 <karim.soumbou@gmail.com>
2023-10-16 06:07:40 -0700

    enemies

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -1,9 +1,14 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-/* more headers goes there */
+/**
+ * Main - entry point  
+ *
+ * Returns: Always 0 (Success)
+ *
+ *
+ */
 
-/* betty style doc for function main goes there */
 int main(void)
 {
 	int n;
