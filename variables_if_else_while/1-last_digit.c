b6c27843113f5b2715bc348acf669114186d79a7
Kayme976 <karim.soumbou@gmail.com>
2023-10-16 06:48:58 -0700

    tu vas marcher non

---
+++ variables_if_else_while/1-last_digit.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+        int n;
+
+        srand(time(0));
+        n = rand() - RAND_MAX / 2;
+        if (n >= 5)
+        {
+		printf("%d greater than 5\n", n);
+        }
+        if (n == 0)
+        {
+                printf("%d is 0 and is 0\n", n);
+        }
+        if (n < 6 != 0)
+        {
+                printf("%d is less than 6 and not 0\n", n);
+        }
+        return (0);
+}
