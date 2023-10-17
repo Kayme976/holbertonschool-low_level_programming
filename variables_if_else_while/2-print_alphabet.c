b0c9127b8f7c0ed7321ccd1b1c4950f1f8c9c5c0
Kayme976 <karim.soumbou@gmail.com>
2023-10-17 01:18:08 -0700

    alphabet

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always (Success)
+ *
+ *
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'a' ; c <= 'z'; c++)
+	putchar(c);	
+	return (0);
+}
