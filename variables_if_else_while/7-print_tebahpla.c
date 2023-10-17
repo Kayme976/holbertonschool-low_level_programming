67378b50700491b13ded6895f32be52cafc78eed
Kayme976 <karim.soumbou@gmail.com>
2023-10-17 07:50:42 -0700

    souris

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always (Success)
+ *
+ *
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'a' ; c <= 'z'; c++)
+		putchar(c);
+	putchar('\n');
+	return (0);
+}
