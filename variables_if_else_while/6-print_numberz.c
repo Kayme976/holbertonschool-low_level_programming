020245b6db841994eb5ab9238783ebff2e10a3cf
Kayme976 <karim.soumbou@gmail.com>
2023-10-17 06:41:16 -0700

    calme

--- variables_if_else_while/6-print_numberz.c
+++ variables_if_else_while/6-print_numberz.c
@@ -10,17 +10,10 @@
  */
 int main(void)
 {
-	int x;
+        int x;
 
-	for (x = 0; x < 10; x++)
-	{
-		putchar(x +'0');
-		if (x < 9)
-		{
-			putchar(',');
-			putchar(' ');
-		}
-	}
-	putchar('\n');
-	return (0);
+        for (x = 0; x < 10; x++)
+        putchar(x);
+        putchar('\n');
+        return (0);
 }
