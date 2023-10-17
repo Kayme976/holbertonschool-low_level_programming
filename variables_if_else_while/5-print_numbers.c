aa9516eaf3e6eca45c10f241bf21dbb4c362e63c
Kayme976 <karim.soumbou@gmail.com>
2023-10-17 07:23:02 -0700

    sosh

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always (Success)
+ *
+ *
+ */
+int main(void)
+{
+        char c;
+
+        for (c = '0'; c <= '9'; c++)
+        {
+                putchar(c);
+        }
+        putchar('\n');
+        return (0);
+}
