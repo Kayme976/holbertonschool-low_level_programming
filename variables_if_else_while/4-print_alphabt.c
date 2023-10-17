fe1458d41e75ad7986ab4f885738de24cd85c52e
Kayme976 <karim.soumbou@gmail.com>
2023-10-17 05:42:04 -0700

    je

--- variables_if_else_while/4-print_alphabt.c
+++ variables_if_else_while/4-print_alphabt.c
@@ -10,16 +10,19 @@
  */
 int main(void)
 {
-	char (c, e, q);
+	char c, e, q;
 
-	e = 'e'
-	q = 'q'
+	e = 'e';
+	q = 'q';
 	
 	for (c = 'a' ; c <= 'z'; c++)
 	{
-	if (c != e && c != q)
-	putchar(c);
+		if (c != e && c != q)
+		{
+			putchar(c);
+		}
 	}
+
 	putchar('\n');
 	return (0);
 }
