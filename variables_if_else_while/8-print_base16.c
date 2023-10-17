786e2cebb077382978a10d7448172a5f2ca28a1c
Kayme976 <karim.soumbou@gmail.com>
2023-10-17 14:17:48 -0700

    wesh les gang

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always (Success)
+ *
+ *
+ */
+int main(void)
+{
+	char c;
+
+	for(c = '0'; c <= '9'; c++)
+	{
+		putchar(c);
+	}
+
+	for (c = 'a'; c <= 'f'; c++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
