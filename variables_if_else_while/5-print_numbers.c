23cddadc2b0e37956aad76f39c2917e7ceb9431a
Kayme976 <karim.soumbou@gmail.com>
2023-10-17 07:29:42 -0700

    soshe

--- variables_if_else_while/5-print_numbers.c
+++ variables_if_else_while/5-print_numbers.c
@@ -10,12 +10,12 @@
  */
 int main(void)
 {
-        char c;
-
-        for (c = '0'; c <= '9'; c++)
-        {
-                putchar(c);
-        }
-        putchar('\n');
-        return (0);
+	char c;
+	
+	for (c = '0'; c <= '9'; c++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
 }
