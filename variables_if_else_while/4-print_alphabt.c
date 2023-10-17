b593c48634bc3e6b48312996315fab8d069d2309
Kayme976 <karim.soumbou@gmail.com>
2023-10-17 02:56:40 -0700

    alphabet 2

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always (Success)
+ *
+ *
+ */
+int main(void)
+{
+	char low, e, q;
+
+	e = 'e'
+	q = 'q'
+	
+	for (c = 'a' ; c <= 'z'; c++)
+	{
+	if (low != e && low != q)
+	putchar(low);
+	}
+	putchar('\n');
+	return (0);
+}
