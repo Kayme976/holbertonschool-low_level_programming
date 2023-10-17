206de14c36f5b53ff970b9344cb989538cdebc48
Kayme976 <karim.soumbou@gmail.com>
2023-10-17 14:48:25 -0700

    enfin

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <tmie.h>
+#include <stdio.h>
+/**
+ * main - Entry point 
+ *
+ * Return: Always (Success)
+ *
+ *
+ */
+int main(void)
+{
+	int x;
+	for (x = '0' ; x <= '9'; x++)
+	{
+		putchar(x);
+	}
+	putchar('\n');
+	return (0);
+}
