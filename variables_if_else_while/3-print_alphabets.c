6b9503abba93e72a7c74c02fa468d328e69e1b35
Kayme976 <karim.soumbou@gmail.com>
2023-10-17 01:36:30 -0700

    en maj

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always (Success)
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'a' ; c <= 'z'; c++)
+	putchar(c);
+	putchar('\n');
+	for ( c= 'A' : c <= 'Z'; c++)
+	putchar(c)
+	putchar('\n');
+	return(0)
+}
