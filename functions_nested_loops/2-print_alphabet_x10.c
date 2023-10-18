c3252dae7c0ec8f607757d6858f094c3e5ccf56b
Kayme976 <karim.soumbou@gmail.com>
2023-10-18 14:07:36 -0700

    ghdif

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -4,7 +4,7 @@
 #include "main.h"
 /**
  * print_alphabet_x10 - Make alphabet x10 times
- *Return: void 
+ *Return: void
  *
  *
  *
@@ -14,11 +14,12 @@ void print_alphabet_x10(void)
 	char c;
 	int i = 0;
 	while (i <= 9)
+
 	{
-        for (c = 'a'; c <= 'z'; c++)
-        {
-		_putchar(c);
-	}
+		for (c = 'a'; c <= 'z'; c++)
+		{
+			_putchar(c);
+		}
 		_putchar('\n');
 		i++;
 
