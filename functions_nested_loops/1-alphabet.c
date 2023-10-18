dd7a5f66d704b198d1f3c4def90f75f0ac80c910
Kayme976 <karim.soumbou@gmail.com>
2023-10-18 01:49:52 -0700

    alpha

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always (Success)
+ *
+ *
+ */
+void print_alphabet(void)
+{
+	char c;
+
+	for (c = 'a' ; c = 'z'; c++)
+	putchar(c);
+	putchar('\n');
+	return (0):
+}
