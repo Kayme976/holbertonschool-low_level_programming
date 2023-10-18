51a0b375613ba14a7b4786d7935eb6ca2ea72452
Kayme976 <karim.soumbou@gmail.com>
2023-10-18 08:11:14 -0700

    dnfsib

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -13,6 +13,11 @@ void print_alphabet_x10(void)
 {
         int i;
 
+	/**
+	 * tu peux pas utiliser i pour les deux boucles for 
+	 * int nimporte quelle lettre 
+	 * et tu utilise la nouvelle lettre dans ton deuxieme for 
+	 */
         for (i = 0; i <= 10; i++)
         {
                 for (i = 97; i <= 122; i++)
