afdb6904ff67e10bbf3776427663d934bb300182
Kayme976 <karim.soumbou@gmail.com>
2023-10-18 06:56:18 -0700

    glah

---
+++ functions_nested_loops/2-print-alphabet_x10.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_alphabet - program that prints _putchar, followed by new line
+ *
+ *
+ *
+ *
+ */
+void print_alphabet_x10(void);
+{
+	int i;
+
+	for (i = 0; i = 10; i++)
+	{
+		for (i = 97; i = 122; i++)
+		{
+			_putchar(i);
+		}
+		_putchar('\n')
+	}
+}
