7b96e5337b0033a86e67680b8dad6504bd395f66
Kayme976 <karim.soumbou@gmail.com>
2023-10-19 14:53:42 -0700

    gkf

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * jack_bauer - Prints every minute of the days of Nidi jsais plus quoi 
+ * jack_bauer - Prints every minute of the days of Nidi jsais plus quoi
  * Return: ...
  *
  */
