b0a23db817ed3c3a364fb90aa677f3780cb4fa9f
Kayme976 <karim.soumbou@gmail.com>
2023-10-19 02:03:07 -0700

    fjdk

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -5,7 +5,7 @@
 #include "ctype.h"
 /**
  * _islower - Checks for lowercase character
- * %c : The character to be checked
+ * @c: The character to be checked
  * Return: 1 for lowercase character or 0 for anything else
  *
  *
