d341be90461596220567f00b2ab079478770f683
Kayme976 <karim.soumbou@gmail.com>
2023-10-19 13:03:55 -0700

    jkd

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -2,7 +2,7 @@
 #include <ctype.h>
 /**
  * print_sign - Prints the sign of a number
- * @c: The number to be checked
+ * @n: The number to be checked
  * Return: 1 for positive num, -1 for negative num or zero for anything else
  */
 
