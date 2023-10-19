5decf10ca9207ff1f876c239492f4a11926596f6
Kayme976 <karim.soumbou@gmail.com>
2023-10-19 02:20:34 -0700

    fkd

--- functions_nested_loops/4-isalpha.c
+++ functions_nested_loops/4-isalpha.c
@@ -1,18 +1,12 @@
-#include <stdlib.h>
-#include <time.h>
-#include <stdio.h>
 #include "main.h"
-#include <ctype.h>
 /**
  * _isalpha - Checks for alphabetic character
  * @c: The charachter to be checked
  * Return: 1 for alphabetic character or 0 for anything else 
  */
-
 int isalpha(int c);
-
 {
-	if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
+	if (c >= 97 && c <= 122)
 		return (1);
 	else
 		return (0);
