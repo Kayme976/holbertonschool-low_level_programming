adaec5af87b6afb5dd28901cae74aeca55e92f3d
Kayme976 <karim.soumbou@gmail.com>
2023-10-19 12:17:11 -0700

    fdsdk

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <ctype.h>
+/**
+ * _isalpha - Checks for alphabetic character
+ * @c: The charachter to be checked
+ * _isalpha checks for alphabetic character
+ * Return: 1 for alphabetic character or 0 for anything else
+ */
+
+int print _sign(int n)
+
+{
+	if (n > 0)
+	{
+		_putchar(43);
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar(45);
+		return (-1);
+	}
+	else
+	{
+		_putchar(48);
+		return (0);
+	}
+}
