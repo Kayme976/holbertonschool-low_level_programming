62c8620322aff3897b5c7180672fbb195c424c17
Kayme976 <karim.soumbou@gmail.com>
2023-10-19 02:28:28 -0700

    ksj

--- functions_nested_loops/4-isalpha.c
+++ functions_nested_loops/4-isalpha.c
@@ -4,7 +4,7 @@
  * @c: The charachter to be checked
  * Return: 1 for alphabetic character or 0 for anything else 
  */
-int isalpha(int c);
+int _isalpha(int c);
 {
 	if (c >= 97 && c <= 122)
 		return (1);
