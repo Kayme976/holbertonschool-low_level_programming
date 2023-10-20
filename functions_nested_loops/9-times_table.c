77b65c9ef610b4d180c537d90f7a4b532d848e7e
Kayme976 <karim.soumbou@gmail.com>
2023-10-20 05:20:09 -0700

    nrjd

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -17,28 +17,30 @@ void times_table(void)
 {
 	int c;
 	int l;
-	int r;
-	int u;
-	int d;
 
-	for (c = 0; c < 10; c++)
+	for (c = 0; c <= 9; c++)
 	{
-		for (l = 0; l <= 9;l++)
-		{
-			r=c*l;
-			d=r/10;
-			u=r%10;
-			if (d != 0)
-			{	_putchar((r/10)+'0');
-				_putchar((r%/10)+'0');
-				_putchar(d+'0');
-				_putchar(u+'0');
-				_putchar(44);
-				_putchar(32);
-				_putchar(32);
-			}
-			_putchar('\n');
-		}
-	}
+		_putchar('0');
 
+		for (l = 1 ; l <= 9;l++)
+		{
+			int r = c * l;
+
+			_putchar(',');
+			_putchar(' ');
+
+			if (r < 10)
+			{
+				_putchar(' ');
+				_putchar(r = '0');
+
+			}
+			else
+			{
+				_putchar((r/10)+'0');
+				_putchar((r%10)+'0');
+			}
+		}
+		_putchar('\n');
+	}
 }
