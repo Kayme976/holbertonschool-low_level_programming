64ddb2a9e1109818027ebec09748bea9969b32a3
Kayme976 <karim.soumbou@gmail.com>
2023-10-20 04:20:57 -0700

    d

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,48 @@
+#include "main.h"
+
+/**
+ * times_table - Prints the 9 times table starting with 0.
+ *
+ * Return: Always 0
+ *
+ * c = colonne
+ * l = ligne
+ * r = resultat
+ * u = unité
+ * d = dizaine
+ *
+ */
+
+void times_table(void)
+{
+	int c;
+	int l;
+	int r;
+	int u;
+	int d;
+
+	for (c = 0; c < 10; c++)
+	{
+		for (l = 0; l <= 9;l++)
+		{
+			r=c*l;
+			d=r/10;
+			u=r%10;
+			if (d != 0)
+			{	_putchar(d+'0');
+				_putchar(u+'0');
+				_putchar(44);
+				_putchar(32);
+				_putchar(32);
+			}
+			else
+			{
+				_putchar(u+'0');
+				_putchar(44);
+				_putchar(32);
+				_putchar(32);
+			}
+				_putchar('\n');
+	}
+
+}
