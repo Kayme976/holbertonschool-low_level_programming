1405fd5264b496faa5fa48df9a7367ab2a72cf34
Kayme976 <karim.soumbou@gmail.com>
2023-10-21 10:10:55 -0700

    Battery Street

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -1,5 +1,5 @@
 #include "main.h"
-#include <studio.h>
+#include <stdio.h>
 
 /**
  * print_to_98 - prints all natural numbers from n to 98
@@ -7,6 +7,7 @@
  */
 
 void print_to_98(int n)
+
 {
 	if (n <= 98)
 	{
