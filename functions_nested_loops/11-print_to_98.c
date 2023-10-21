b40187cf5fac9d1559ea789f6f759caceb0335b8
Kayme976 <karim.soumbou@gmail.com>
2023-10-21 09:54:40 -0700

    Battery Street

---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * print_to_98 - prints all natural numbers from n to 98
+ * @n: every numbers
+ */
+
+void print-to_98 (int n )
+{
+	if ( n <= 98 )
+	{
+		for (; n<= 98 ; n++)
+		{
+			printf ("%d", n);
+
+			if ( n != 98 )
+			{
+				printf (',');
+			}
+		}
+	}
+	else
+	{
+		for (; n => 98 ; n--)
+		{
+			printf("%d", n);
+			if (n != 98)
+			{
+				printf(',');
+			}
+		}
+	}
+	printf("\n");
+}
