32074a412ab11be0cc695d3c5158ffa27039acab
Kayme976 <karim.soumbou@gmail.com>
2023-10-21 09:57:50 -0700

    Battery Street

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -5,28 +5,28 @@
  * @n: every numbers
  */
 
-void print-to_98 (int n )
+void print_to_98(int n)
 {
-	if ( n <= 98 )
+	if (n <= 98)
 	{
-		for (; n<= 98 ; n++)
+		for (; n <= 98 ; n++)
 		{
-			printf ("%d", n);
+			printf("%d", n);
 
-			if ( n != 98 )
+			if (n != 98)
 			{
-				printf (',');
+				printf(", ");
 			}
 		}
 	}
 	else
 	{
-		for (; n => 98 ; n--)
+		for (; n >= 98 ; n--)
 		{
 			printf("%d", n);
 			if (n != 98)
 			{
-				printf(',');
+				printf(", ");
 			}
 		}
 	}
