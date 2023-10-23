12f0fa6a05ec1c2ee0bd1fa788e99acb94f5319e
Kayme976 <karim.soumbou@gmail.com>
2023-10-23 16:05:16 -0700

    Collaboration multi

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * mul - that multiplies two integers.
+ * @a: is my var that's going to be multiplied by b
+ * @b: is my var that's going to be multiplied by a
+ * Return: multiplication
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
