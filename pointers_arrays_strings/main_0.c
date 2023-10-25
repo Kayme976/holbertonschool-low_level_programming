a260f7718d0a365a75958554fcf513b7a6b32d44
Kayme976 <karim.soumbou@gmail.com>
2023-10-25 05:57:42 -0700

    winning

---
+++ pointers_arrays_strings/main_0.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+void puts_half(char *);
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char *str;
+
+	str = "Holberton!";
+	puts_half(str);
+	return (0);
+}
