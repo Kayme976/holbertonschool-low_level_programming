c11cf60c5c6829ca110f803608045088a48793e2
Kayme976 <karim.soumbou@gmail.com>
2023-10-26 06:24:53 -0700

    3strcmp

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -5,7 +5,7 @@
  * @s1: first string to compare
  * @s2: second string to compare
  * Return: less than 0 if k1 is less than k2
- * more than 0 if less thna 
+ * more than 0 if less thna
  *
  */
 int _strcmp(char *s1, char *s2)
