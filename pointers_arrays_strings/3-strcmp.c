19bce278a4c0d4e49194c4262c146f0d0f5de3de
Kayme976 <karim.soumbou@gmail.com>
2023-10-26 06:17:25 -0700

    3strcmp

---
+++ pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _strcmp - that compares two strings
+ * @s1: first string to compare
+ * @s2: second string to compare
+ * Return: less than 0 if k1 is less than k2
+ * more than 0 if less thna 
+ *
+ */
+int _strcmp(char *s1, char *s2)
+{
+	while (*s1 == *s2)
+	{
+		if (*s1 == *s2)
+		{
+			return (0);
+		}
+		s1++;
+		s2++;
+	}
+	return (*s1 - *s2);
+}
