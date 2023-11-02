a0c0b1c1ec0ea33046867e3c4f6198b0fb8bdef2
Kayme976 <karim.soumbou@gmail.com>
2023-11-02 16:41:34 -0700

    0 recursion

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -4,7 +4,7 @@
  * _puts_recrusion - that prints a string, followed by a new line
  * @s: string to print
  */
-void _puts_recursion(char * s)
+void _puts_recursion(char *s)
 {
 	if (*s == '\0')
 	{
