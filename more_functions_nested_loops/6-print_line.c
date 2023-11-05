ef52f6f21ea73096b2620e8d89cbfb9fb524d4a9
Kayme976 <karim.soumbou@gmail.com>
2023-11-05 13:01:26 -0800

    tout le reste

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_line - that printing a line with  _
+ * @n: number of times '_' should be printed.
+ */
+void print_line(int n)
+{
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		while (n > 0)
+		{
+			_putchar('_');
+			n--;
+		}
+		_putchar('\n');
+	}
+}
