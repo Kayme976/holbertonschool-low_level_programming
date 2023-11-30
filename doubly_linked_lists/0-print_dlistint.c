7fdea31c86c3c07dfa57476cc97736ca3c55812b
Kayme976 <karim.soumbou@gmail.com>
2023-11-30 07:27:20 -0800

    update

--- doubly_linked_lists/0-print_dlistint.c
+++ doubly_linked_lists/0-print_dlistint.c
@@ -16,10 +16,6 @@ size_t print_dlistint(const dlistint_t *h)
 	if (h == NULL)
 		return (count);
 
-	while (h->prev != NULL)
-		h = h->prev;
-
-	while (h != NULL)
 	{
 		printf("%d\n", h->n);
 		count++;
