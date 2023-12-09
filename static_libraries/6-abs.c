ccf6403b3c7943b1eeb4296fa406c3ec174cbb3a
Kayme976 <karim.soumbou@gmail.com>
2023-12-09 08:00:14 -0800

    update every project

---
+++ static_libraries/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _abs - that computes the absolute value of an integer
+ * @n: the int to chek
+ * Return: the absolute value of int
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+	{
+		return (n);
+	}
+	return (-n);
+}
--- static_libraries/main.h
+++ static_libraries/main.h
@@ -1,3 +1,7 @@
+#ifndef MAIN_H
+#define MAIN_h
+
+
 int _putchar(char c);
 int _islower(int c);
 int _isalpha(int c);
@@ -18,3 +22,6 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
+
+
+#endif
