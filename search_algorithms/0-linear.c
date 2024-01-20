6117f377e5e906361c5ed75d5b7f3951673454fe
Kayme976 <karim.soumbou@gmail.com>
2024-01-20 12:17:40 -0800

    update3

--- search_algorithms/0-linear.c
+++ search_algorithms/0-linear.c
@@ -14,7 +14,7 @@ int linear_search(int *array, size_t size, int value)
 	if (array)
 		for (i = 0; i < size; i++)
 		{
-			printf("value checked array[%ld] = [%d]\n", i, array[i]);
+			printf("Value checked array[%ld] = [%d]\n", i, array[i]);
 			if (array[i] == value)
 				return (i);
 		}
